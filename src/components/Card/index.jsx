66ca4423803e786eb4d9a6396dfec28452ea77b0
vdallabona <vinicius.dallabona@gmail.com>
2024-09-13 21:20:44 -0300

    atualizações de episodios

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,9 +1,13 @@
 import './styles.css'
 
 export default function Card({ data: personagem }){
+    let imagem = personagem.image;
+    if (imagem === null){
+        imagem = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNK7-n-r_w_qCEIjsnu8VXMBamUkSmLUr9Eg&s'
+    }
     return(
         <div className='card char'>
-            <img className='animation1' src={personagem.image} alt={personagem.name} /> 
+            <img className='animation1' src={imagem} alt={personagem.name} /> 
             <h2>{personagem.name}</h2>
 
             <div className='char-info'>
